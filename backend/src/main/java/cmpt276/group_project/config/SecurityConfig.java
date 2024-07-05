c9e5b7d086e720e7af409b955c30e77e4f5a0ba9
CheezyTaco <cheteoong@gmail.com>
2024-07-04 20:44:25 -0700

    renderURL

--- backend/src/main/java/cmpt276/group_project/config/SecurityConfig.java
+++ backend/src/main/java/cmpt276/group_project/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package cmpt276.group_project.config;
 
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,11 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     @Value("${cors.allowedOrigins}")
     private String allowedOrigins;
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
@@ -30,10 +31,12 @@ public class SecurityConfig{
         return http.build();
     }
 
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
