33352ebc0b845cfb18c37263852c6d76b8a8c65d
CheezyTaco <cheteoong@gmail.com>
2024-07-05 08:13:35 -0700

    webconfig

---
+++ backend/src/main/java/cmpt276/group_project/config/WebConfig.java
@@ -0,0 +1,19 @@
+package cmpt276.group_project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://cmpt276-groupprojecttest.onrender.com", "http://localhost:3000", "http://10.0.0.14:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
